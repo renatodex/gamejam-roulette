9e251dc7df92f465e3b55ffaac0c3f50c707bc5e
renatodex <renatodex@gmail.com>
2014-09-14 20:37:15 -0300

    Fixe paths

--- lib/gamejam-roulette.rb
+++ lib/gamejam-roulette.rb
@@ -4,13 +4,13 @@ require 'json'
 
 if defined?(Motion::Project::Config)
 	Motion::Project::App.setup do |app|
-	  Dir.glob(File.join(File.dirname(__FILE__), '/lib/**/*.rb')).each do |file|
+	  Dir.glob(File.join(File.dirname(__FILE__), '/**/*.rb')).each do |file|
 	    app.files.unshift(file)
 	  end
 	end
 else
-	require_relative 'settings'
-	Dir[File.dirname(__FILE__) + '/lib/**/*.rb'].each { |file| require_relative file }
+	require_relative '../settings'
+	Dir[File.dirname(__FILE__) + '/**/*.rb'].each { |file| require_relative file }
 end
 
 module Gamejam
