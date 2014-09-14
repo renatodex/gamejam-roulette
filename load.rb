5ee7515bc3c944e5ac497d9279fa57d021158306
renatodex <renatodex@gmail.com>
2014-09-14 20:17:41 -0300

    1.5 -> Added RubyMotion support

--- load.rb
+++ load.rb
@@ -1,3 +1,11 @@
 require 'json'
 require_relative 'settings'
 Dir[File.dirname(__FILE__) + '/lib/**/*.rb'].each { |file| require_relative file }
+
+if defined?(Motion::Project::Config)
+	Motion::Project::App.setup do |app|
+	  Dir.glob(File.join(File.dirname(__FILE__), '/lib/**/*.rb')).each do |file|
+	    app.files.unshift(file)
+	  end
+	end
+end
