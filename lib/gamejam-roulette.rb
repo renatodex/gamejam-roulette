db13cff3c62cc395ca8c5d9a418f5c1a5b991bbf
renatodex <renatodex@gmail.com>
2014-09-14 20:32:43 -0300

    Added 1.7

--- lib/gamejam-roulette.rb
+++ lib/gamejam-roulette.rb
@@ -1,4 +1,17 @@
-require_relative File.join(File.dirname(__FILE__), "../load.rb")
+#require_relative File.join(File.dirname(__FILE__), "../load.rb")
+
+require 'json'
+
+if defined?(Motion::Project::Config)
+	Motion::Project::App.setup do |app|
+	  Dir.glob(File.join(File.dirname(__FILE__), '/lib/**/*.rb')).each do |file|
+	    app.files.unshift(file)
+	  end
+	end
+else
+	require_relative 'settings'
+	Dir[File.dirname(__FILE__) + '/lib/**/*.rb'].each { |file| require_relative file }
+end
 
 module Gamejam
 	module Roulette
